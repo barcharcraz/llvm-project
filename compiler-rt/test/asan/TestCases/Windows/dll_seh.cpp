eb1af407d9368d2df365917896f0381ace3a640a
Matthew G McGovern <mamcgove@microsoft.com>
2020-06-15 13:50:52 -0700

    dll seh is not xfail on x64 anymore

--- compiler-rt/test/asan/TestCases/Windows/dll_seh.cpp
+++ compiler-rt/test/asan/TestCases/Windows/dll_seh.cpp
@@ -6,7 +6,7 @@
 //
 // RUN: %clang_cl_asan -GS- -LD -Od %s -Fe%t.dll
 // RUN: %run %t %t.dll
-// XFAIL: msvc-host && asan-dynamic-runtime
+// XFAIL: msvc-host && asan-dynamic-runtime && asan-32-bits
 #include <windows.h>
 #include <assert.h>
 #include <stdio.h>
