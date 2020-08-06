0b82a654e469499c030c15752f1e33825b8af9ee
Brijesh Rakholia <brrakhol@microsoft.com>
2020-08-06 22:05:30 +0000

    Merged PR 265968: Fix strdup tests to be compatible with extra underscores
    Fix strdup tests to be compatible with extra underscores

--- compiler-rt/test/asan/TestCases/Windows/intercept_strdup.cpp
+++ compiler-rt/test/asan/TestCases/Windows/intercept_strdup.cpp
@@ -23,7 +23,7 @@ int main() {
   //
   // The first frame is our wrapper normally but will be malloc in the dynamic
   // config.
-  // CHECK:   #0 {{.*}} in {{malloc|__asan_wrap_strdup}}
+  // CHECK:   #0 {{.*}} in {{malloc|__asan_wrap_strdup|__asan_wrap__strdup}}
   //
   // The local call to _strdup above may be the second or third or fourth frame depending
   // on whether we're using the dynamic or debug config.
