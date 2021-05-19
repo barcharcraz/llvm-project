d7c37a92c036378c8117623d8f16d9cd41aa320d
Curtis Bezault <cubezaul@microsoft.com>
2021-05-19 17:34:02 +0000

    Merged PR 322160: Avoid potential deadlock
    Avoid potential deadlock in the scoped lock

--- compiler-rt/lib/asan/asan_win_scoped_lock.h
+++ compiler-rt/lib/asan/asan_win_scoped_lock.h
@@ -10,16 +10,18 @@ class RecursiveScopedLock {
   explicit RecursiveScopedLock(__sanitizer::SpinMutex &_lock,
                                __sanitizer::atomic_uint32_t &_thread_id)
       : lock(_lock), thread_id(_thread_id), serialized(false) {
-    if (atomic_load_relaxed(&thread_id) != GetCurrentThreadId()) {
-      lock.Lock();
-      atomic_store_relaxed(&thread_id, GetCurrentThreadId());
+    if (atomic_load(&thread_id, __sanitizer::memory_order_seq_cst) !=
+        GetCurrentThreadId()) {
       serialized = true;
+      lock.Lock();
+      atomic_store(&thread_id, GetCurrentThreadId(),
+                   __sanitizer::memory_order_relaxed);
     }
   }
 
   ~RecursiveScopedLock() {
     if (serialized) {
-      atomic_store_relaxed(&thread_id, 0);
+      atomic_store(&thread_id, 0, __sanitizer::memory_order_seq_cst);
       lock.Unlock();
     }
   }
