96e6a2a3407954e44ab73a5da5f1538086fd2060
Kemi Porto <kemipor@microsoft.com>
2022-08-01 15:09:10 -0700

    [LLVM14 Upgrade] Fixes by looking at diff
    undoing change

--- compiler-rt/test/asan/TestCases/Windows/dll_stack_use_after_return.cpp
+++ compiler-rt/test/asan/TestCases/Windows/dll_stack_use_after_return.cpp
@@ -1,6 +1,3 @@
-
-
-
 // RUN: %clang_cl_asan -Od %p/dll_host.cpp -Fe%t
 // RUN: %clang_cl_asan -LD -Od %s -Fe%t.dll
 // RUN: %env_asan_opts=detect_stack_use_after_return=1 not %run %t %t.dll 2>&1 | FileCheck %s
