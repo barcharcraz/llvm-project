e2e83a4510bdb7c905b90f6bacac0bc887e8fb5b
Kemi Porto <kemipor@microsoft.com>
2022-08-18 16:58:00 -0700

    [LLVM14 Upgrade] __attribute__((noinline)) to __declspec(noinline)

--- compiler-rt/test/asan/TestCases/strncpy-overflow.cpp
+++ compiler-rt/test/asan/TestCases/strncpy-overflow.cpp
@@ -13,7 +13,11 @@
 // strncpy below (which otherwises writes to dead storage).  We
 // need the read to be out-of-line to prevent memory forwarding
 // from making the memory dead again.
-int sink_memory(int N, char *p) __attribute__((noinline));
+#ifdef _MSC_VER
+  __declspec(noinline) int sink_memory(int N, char *p);
+#else
+  int sink_memory(int N, char *p) __attribute__((noinline));
+#endif
 int sink_memory(int N, char *p) {
   int sum = 0;
   for (int i = 0; i < N; i++)
