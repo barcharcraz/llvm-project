2c0f44c5a23a6b42c193cf40f71959802d5832a4
Charlie Barto <chbarto@microsoft.com>
2024-04-16 10:59:54 -0700

    [LLVM 19] Allow more flexibility in stack frames in debug mode

--- compiler-rt/test/asan/TestCases/debug_stacks.cpp
+++ compiler-rt/test/asan/TestCases/debug_stacks.cpp
@@ -64,10 +64,10 @@ int main() {
   // CHECK: WRITE of size 1 at 0x{{.*}}
   // CHECK: freed by thread T0 here:
   // CHECK: #0 [[FREE_FRAME_0]]
-  // CHECK: #1 0x{{.*}} in func2
+  // CHECK: #{{[12]}} 0x{{.*}} in func2
   // CHECK: previously allocated by thread T0 here:
   // CHECK: #0 [[ALLOC_FRAME_0]]
-  // CHECK: #1 [[ALLOC_FRAME_1]]
+  // CHECK: #{{[12]}} [[ALLOC_FRAME_1]]
 
   return 0;
 }
