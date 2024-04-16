40cbb79efca7c65d18b4b8c7dcd51649f4ce08d4
Charlie Barto <chbarto@microsoft.com>
2024-04-16 11:28:35 -0700

    dll_host.cpp: use FormatMessageA correctly

--- compiler-rt/test/asan/TestCases/Windows/dll_host.cpp
+++ compiler-rt/test/asan/TestCases/Windows/dll_host.cpp
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
 
     FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
                        FORMAT_MESSAGE_IGNORE_INSERTS,
-                   NULL, err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buf, 0,
+                   NULL, err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&buf, 0,
                    NULL);
 
     printf("Error: %s\n", buf);
