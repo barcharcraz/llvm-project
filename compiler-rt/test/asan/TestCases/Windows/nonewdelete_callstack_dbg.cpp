9b9c7c7245ad58b06eb93822d7f3ccdebe215d37
Dmitry Kobets <dmitrykobets@microsoft.com>
2024-05-15 20:32:02 +0000

    Merged PR 546502: Create copy of ASan RT but without new/delete overrides
    MSVC PR: !544826
    
    The windows MORSE team has some libs that redefine new/delete, and this collides with ASan's own new/delete definitions which come as part of `clang_rt.asan_dynamic-<arch>.lib`.
    The agreed-upon solution is to simply create a copy of this RT lib (`clang_rt.asan_nonewdelete_dynamic-<arch>.lib`, and `clang_rt.asan_dbg_nonewdelete_dynamic-<arch>.lib`), but to not include the new/delete overrides in the copy.
    
    The primary differences between the nonewdelete .lib and existing .lib are:
    - different callstacks in ASan error output: operator new/delete and related allocation functions will be arranged differently in these callstacks.
    - alloc-dealloc-mismatch is not detected
    - new-delete-type-mismatch is not detected
    
    This PR adds testcases to validate the above differences.
    
    Related work items: #1369778

---
+++ compiler-rt/test/asan/TestCases/Windows/nonewdelete_callstack_dbg.cpp
@@ -0,0 +1,32 @@
+// Without ASan's new/delete overrides, the callstack looks slightly different.
+// This testcase matches against this difference. 
+// (See testcase `double_operator_delete.cpp` for the version with new/delete overrides)
+// REQUIRES: asan-debug-runtime
+
+// RUN: %clang_cl_asan -Od %s -Fe%t %nonewdelete_link
+// RUN: not %run %t 2>&1 | FileCheck %s
+
+#include <malloc.h>
+
+int main() {
+  int *x = new int[42];
+  delete [] x;
+  delete [] x;
+// CHECK: AddressSanitizer: attempting double-free on [[ADDR:0x[0-9a-f]+]]
+// CHECK-NEXT: {{#0 .* _free_dbg}}
+// CHECK-NEXT: {{#1 .* operator delete}}
+// CHECK-NEXT: {{#2 .* operator delete[[]]}}
+// CHECK-NEXT: {{#3 .* main .*nonewdelete_callstack_dbg.cpp}}:[[@LINE-5]]
+// CHECK: [[ADDR]] is located 0 bytes inside of 168-byte region
+// CHECK-LABEL: freed by thread T0 here:
+// CHECK-NEXT: {{#0 .* _free_dbg}}
+// CHECK-NEXT: {{#1 .* operator delete}}
+// CHECK-NEXT: {{#2 .* operator delete[[]]}}
+// CHECK-NEXT: {{#3 .* main .*nonewdelete_callstack_dbg.cpp}}:[[@LINE-12]]
+// CHECK-LABEL: previously allocated by thread T0 here:
+// CHECK-NEXT: {{#0 .* malloc}}
+// CHECK-NEXT: {{#1 .* operator new}}
+// CHECK-NEXT: {{#2 .* operator new[[]]}}
+// CHECK-NEXT: {{#3 .* main .*nonewdelete_callstack_dbg.cpp}}:[[@LINE-18]]
+  return 0;
+}
---
+++ compiler-rt/test/asan/TestCases/Windows/nonewdelete_callstack_rel.cpp
@@ -0,0 +1,28 @@
+// Without ASan's new/delete overrides, the callstack looks slightly different.
+// This testcase matches against this difference. 
+// (See testcase `double_operator_delete.cpp` for the version with new/delete overrides)
+// REQUIRES: asan-release-runtime
+
+// RUN: %clang_cl_asan -Od %s -Fe%t %nonewdelete_link
+// RUN: not %run %t 2>&1 | FileCheck %s
+
+#include <malloc.h>
+
+int main() {
+  int *x = new int[42];
+  delete [] x;
+  delete [] x;
+// CHECK: AddressSanitizer: attempting double-free on [[ADDR:0x[0-9a-f]+]]
+// CHECK-NEXT: {{#0 .* free}}
+// CHECK-NEXT: {{#1 .* main .*nonewdelete_callstack_rel.cpp}}:[[@LINE-3]]
+// CHECK: [[ADDR]] is located 0 bytes inside of 168-byte region
+// CHECK-LABEL: freed by thread T0 here:
+// CHECK-NEXT: {{#0 .* free}}
+// CHECK-NEXT: {{#1 .* main .*nonewdelete_callstack_rel.cpp}}:[[@LINE-8]]
+// CHECK-LABEL: previously allocated by thread T0 here:
+// CHECK-NEXT: {{#0 .* malloc}}
+// CHECK-NEXT: {{#1 .* operator new}}
+// CHECK-NEXT: {{#2 .* main .*nonewdelete_callstack_rel.cpp}}:[[@LINE-13]]
+  return 0;
+}
+
